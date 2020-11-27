07cf951ceac0f7cc13f04698885bd3771be83887
Sam Foucart <samfoucart25@gmail.com>
2020-11-27 00:59:45 -0500

    Resized Canvas and updated Viewport

--- js/main.js
+++ js/main.js
@@ -86,7 +86,24 @@ function initializeRenderer() {
     var primitiveType = gl.TRIANGLES;
     var offset = 0;
     var count = 3;
+    resize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.drawArrays(primitiveType, offset, count);
+}
+
+function resize(canvas) {
+    // Lookup the size the browser is displaying the canvas.
+    var displayWidth  = canvas.clientWidth;
+    var displayHeight = canvas.clientHeight;
+
+    // Check if the canvas is not the same size.
+    if (canvas.width  != displayWidth ||
+        canvas.height != displayHeight) {
+
+        // Make the canvas the same size
+        canvas.width  = displayWidth;
+        canvas.height = displayHeight;
+    }
 }
 
 initializeRenderer()
